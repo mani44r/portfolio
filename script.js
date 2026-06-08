20de6f6947961ec030c1ff888a5ae40b06c68e7a
RAMINENI MANIKANTA <ugs23123_aids.manikanta@cbit.org.in>
2026-06-08 23:54:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Portfolio Loaded Successfully!");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({
+                behavior: "smooth"
+            });
+    });
+});
